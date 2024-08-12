d82d4c53fd36a93bbcf39568881f28c2b2fabea0
Rachit <rachitg2901@gmail.com>
2024-08-12 09:39:30 +0530

    Eslint added

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"],
+  },
+  {
+    rules: {
+      "no-console": "error",
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+);
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,2 @@
+const h = "Rachit";
 console.log("Hi");
