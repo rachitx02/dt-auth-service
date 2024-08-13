9870180487b0726b15e422354efbe0dc9d9b61a6
Rachit <rachitg2901@gmail.com>
2024-08-13 21:48:01 +0530

    Starting Error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("HI");
+});
+
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  logger.info(`${err} is error ${req} is request is ${res} is response`);
+  next();
 });
 
 export default app;
