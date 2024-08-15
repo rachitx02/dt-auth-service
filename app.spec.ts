c31caa4d3ca907275782ad99c9d786d6127c8c78
Rachit <rachitg2901@gmail.com>
2024-08-15 20:11:00 +0530

    Added Automated Testing

---
+++ app.spec.ts
@@ -0,0 +1,8 @@
+import request from "supertest";
+import app from "./src/app";
+describe("App", () => {
+  it("should return status code 200", async () => {
+    const response = await request(app).get("/").send();
+    expect(response.statusCode).toBe(200);
+  });
+});
